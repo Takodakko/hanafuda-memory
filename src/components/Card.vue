14295fb891ac6974d0a3e3c11c982679473f066e
Takodakko <monchichi@Monchichis-Air.lan>
2024-03-20 15:31:24 -0400

    card flipping working, color for scored cards and tracking of card types, still need scoring and win condition

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed, ref, defineEmits } from 'vue';
+import { computed, ref, defineEmits, PropType } from 'vue';
+
   const props = defineProps({
     imageUrl: {type: String, required: true},
     altText: {type: String, required: true},
-    faceUp: {type: Boolean, required: true},
+    faceUp: {type: null as unknown as PropType<boolean | null>, required: true, validator: (v: boolean | null) => v === true || v === null || v === false},
     id: {type: String, required: true},
     currentCardsUp: {type: Number, required: true},
     month: {type: String, required: true},
@@ -13,28 +14,39 @@ import { computed, ref, defineEmits } from 'vue';
 
   function flipCard() {
     console.log('flipping', props.month);
-    if (isFaceUp.value === true) {
-        isFaceUp.value = false;
-        emit('flipped', {id: props.id, faceUp: false, month: props.month})
+    if (props.faceUp === true) {
+        emit('flipped', {id: props.id, faceUp: true, month: props.month})
     } else {
-        if (props.currentCardsUp < 2) {
-            isFaceUp.value = true;
-            emit('flipped', {id: props.id, faceUp: true, month: props.month}) 
+        if (props.currentCardsUp < 2 && props.faceUp === false) {
+            emit('flipped', {id: props.id, faceUp: false, month: props.month}) 
         } else {
             console.log('not flipping')
         }
     }
   }
 
-const isFaceUp = ref(props.faceUp);
+  const imageSrc = computed(() => {
+    return props.faceUp === true || props.faceUp === null ? props.imageUrl : '../src/assets/cards/card_back.jpg';
+  });
+
+const imageAlt = computed(() => {
+    return props.faceUp === true || props.faceUp === null ? props.altText : 'back of card';
+});
+
+const cardBoxClass = computed(() => {
+    return props.faceUp === null ? 'card-box scored-card' : 'card-box';
+});
   
 </script>
 <template>
-  <div>
-    <img @click="flipCard" class="card" :src="isFaceUp ? props.imageUrl : '../src/assets/cards/card_back.jpg'" :alt="isFaceUp ? props.altText : 'back of card'">
+  <div :class="cardBoxClass">
+    <img @click="flipCard" class="card" :src="imageSrc" :alt="imageAlt">
   </div>
 </template>
 <style scoped>
+  .card-box {
+    border-style: none;
+  }
   .card {
     height: 100px;
     width: 65px;
@@ -42,5 +54,8 @@ const isFaceUp = ref(props.faceUp);
     padding-right: 10px;
     padding-top: 5px;
     padding-bottom: 5px;
+  }
+  .scored-card {
+    background-color: yellow;
   }
 </style>
--- src/components/HanafudaMemory.vue
+++ src/components/HanafudaMemory.vue
@@ -4,102 +4,120 @@ import Card from './Card.vue';
 interface aCard {
     url: string;
     altText: string;
-    faceUp: boolean;
+    faceUp: boolean | null;
     month: string;
     id: string;
 }
 
-// const monthList = {
-//     'january': false,
-//     'february': false,
-//     'march': false,
-//     'april': false,
-//     'may': false,
-//     'june': false,
-//     'july': false,
-//     'august': false,
-//     'september': false,
-//     'october': false,
-//     'november': false,
-//     'december': false,
-// };
+// type months = 'january' | 'february' | 'march' | 'april' | 'may' | 'june' | 'july' | 'august' | 'september' | 'october' | 'november' | 'december';
 
-interface monthList {
+interface MonthList {
     [key: string]: boolean;
 }
 
 const points = computed(() => Math.floor(matchedCards.value / 2));
-const numberOfRows = 4;
-const numberOfColumns = 12;
 const cardList = ['january_crane', 'january_scroll', 'january_plain1', 'january_plain2', 'february_bird', 'february_scroll', 'february_plain1', 'february_plain2', 'march_curtain', 'march_scroll', 'march_plain1', 'march_plain2', 'april_bird', 'april_scroll', 'april_plain1', 'april_plain2', 'may_dock', 'may_scroll', 'may_plain1', 'may_plain2', 'june_butterfly', 'june_scroll', 'june_plain1', 'june_plain2', 'july_boar', 'july_scroll', 'july_plain1', 'july_plain2', 'august_moon', 'august_geese', 'august_plain1', 'august_plain2', 'september_sake', 'september_scroll', 'september_plain1', 'september_plain2', 'october_deer', 'october_scroll', 'october_plain1', 'october_plain2', 'november_man', 'november_bird', 'november_scroll', 'november_thunder', 'december_phoenix', 'december_plain1', 'december_plain2', 'december_plain3'];
 const sampleCardName = cardList[Math.floor(Math.random() * cardList.length)];
 const sampleCard = ref(`/src/assets/cards/${sampleCardName}.jpg`);
-// function flipCard() {
-//     return undefined;
-// }
 
-const blankMonthList = new Map();
-blankMonthList.set('january', []).set('february', []).set('march', []).set('april', []).set('may', []).set('june', []).set('july', []).set('august', []).set('september', []).set('october', []).set('november', []).set('december', []);
+const monthList: MonthList = {
+    'january': false,
+    'february': false,
+    'march': false,
+    'april': false,
+    'may': false,
+    'june': false,
+    'july': false,
+    'august': false,
+    'september': false,
+    'october': false,
+    'november': false,
+    'december': false,
+};
+
 function initializeCards() {
     console.log('initializing...');
-    const deckToUse: aCard[][] = [];
+    const initializedDeck = new Map();
     const cardListCopy = [...cardList];
-    let row: aCard[] = [];
     let currentId = 1;
     while (cardListCopy.length) {
         const index = Math.floor(Math.random() * cardListCopy.length);
         const currentCard = cardListCopy[index];
         const currentCardName = currentCard.replace('_', ' ');
         const monthName = currentCardName.slice(0, currentCardName.indexOf(' '));
-        row.push({
+        const cardId = currentId.toString();
+        const newCard: aCard = {
             url: `/src/assets/cards/${currentCard}.jpg`,
             altText: currentCardName,
             faceUp: false,
             month: monthName,
-            id: currentId.toString()
-        });
-        blankMonthList.get(monthName).push(currentId.toString());
-        if (currentId % numberOfColumns === 0) {
-            deckToUse.push(row);
-            row = [];
-        }
+            id: cardId,
+        };
+        initializedDeck.set(cardId, newCard);
         currentId++;
         cardListCopy.splice(index, 1);
     }
-    console.log(deckToUse.length)
-    return deckToUse;
+    return initializedDeck;
 }
 
+const newDeck = initializeCards();
 
+const currentDeck = ref(newDeck);
 
-const currentDeck = computed(initializeCards);
-const faceDownCards = ref(48);
-const faceUpCards = ref(0);
-const matchedCards = ref(0);
-const matchingMonths = ref(blankMonthList);
-// const canFlipCard = computed({
-//     get: () => ,
-//     set: (val) => !val && faceUpCards.value < 2,
-// });
-// onMounted(initializeCards);
-// onUpdated(initializeCards);
+const faceDownCards = computed(() => {
+    let number = 0;
+    currentDeck.value.forEach((card) => {
+        if (card.faceUp === false) {
+            number += 1;
+        }
+    });
+    return number;
+});
+
+const faceUpCards = computed(() => {
+    let number = 0;
+    currentDeck.value.forEach((card) => {
+        if (card.faceUp === true) {
+            number += 1;
+        }
+    });
+    return number;
+});
+
+const matchedCards = computed(() => {
+    let number = 0;
+    currentDeck.value.forEach((card) => {
+        if (card.faceUp === null) {
+            number += 1;
+        }
+    });
+    return number;
+});
+const matchingMonths = ref(monthList);
 
 function listenForFlip(e: {id: string, faceUp: boolean, month: string}) {
     console.log(e.id, e.faceUp, e.month, 'child flip');
-    console.log(matchingMonths.value)
-    if (e.faceUp) {
-        faceDownCards.value -= 1;
-        faceUpCards.value += 1;
-        if (matchingMonths.value.get(e.month) === true) {
-          matchedCards.value += 2;
-          faceUpCards.value -= 2;
-          matchingMonths.value.set(e.month, false);
+    const flippedCard = currentDeck.value.get(e.id);
+    const currentCardMonth = e.month;
+    if (e.faceUp === false) {
+        if (matchingMonths.value[currentCardMonth] === false) {
+            currentDeck.value.set(e.id, {...flippedCard, faceUp: true});
+            matchingMonths.value[currentCardMonth] = true;
         } else {
-            matchingMonths.value.set(e.month, true);
+            currentDeck.value.set(e.id, {...flippedCard, faceUp: null});
+            matchingMonths.value[currentCardMonth] = false;
+            currentDeck.value.forEach((card) => {
+                if (card.faceUp === true) {
+                    card.faceUp = null;
+                }
+            });
         }
+        
+    } else if (e.faceUp === true) {
+        currentDeck.value.set(e.id, {...flippedCard, faceUp: false});
+        matchingMonths.value[currentCardMonth] = false;
     } else {
-        faceDownCards.value += 1;
-        faceUpCards.value -= 1;
+        console.log('already scored...');
     }
 }
 
@@ -108,10 +126,13 @@ function listenForFlip(e: {id: string, faceUp: boolean, month: string}) {
 <template>
     hi
     {{ points }}
+    {{ faceUpCards }} face up
+    {{ faceDownCards }} face down
+    {{ matchedCards }} matched cards
     <Card month="xanuary" :current-cards-up="faceUpCards" :image-url="sampleCard" :alt-text="sampleCardName.replace('_', ' ')" :face-up="true" id="example"/>
-    <div class="card-container" v-for="row in currentDeck">
-      <div v-for="card in row">
-        <Card :month="card.month" :current-cards-up="faceUpCards" :image-url="card.url" :alt-text="card.altText" :key="card.id" :face-up="card.faceUp" :id="card.id" @flipped="listenForFlip"/>
+    <div class="card-container">
+      <div v-for="[id, card] in currentDeck">
+        <Card :month="card.month" :current-cards-up="faceUpCards" :image-url="card.url" :alt-text="card.altText" :key="id" :face-up="card.faceUp" :id="id" @flipped="listenForFlip"/>
       </div><br>
     </div>
 </template>
@@ -123,5 +144,6 @@ function listenForFlip(e: {id: string, faceUp: boolean, month: string}) {
     flex-wrap: wrap;
     padding: 1px;
     margin: 5px;
+    max-width: 1100px;
   }
 </style>
